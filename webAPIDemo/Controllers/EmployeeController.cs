c38c052e5bc5330659db91ffb97ef9689c2f9bf1
burakguler <burakguler@users.noreply.github.com>
2021-03-01 13:16:28 +0300

    Initial commit

---
+++ webAPIDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using webAPIDemo.Models;
+
+namespace webAPIDemo.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        private List<Employee> listEmployees = new List<Employee>
+       {
+           new Employee{Id=1, ad="Burak",soyAd="Güler"},
+           new Employee{Id=1, ad="Burak",soyAd="Gülmez"},
+           new Employee{Id=1, ad="Burak",soyAd="Gülür"},
+
+       };
+
+        public IEnumerable<Employee> GetEmployees()
+        {
+            return listEmployees;
+        }
+
+        // GET: api/Employee
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET: api/Employee/5
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        // POST: api/Employee
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Employee/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Employee/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ webAPIDemo/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webAPIDemo.Models
+{
+    public class Employee
+    {
+        public int Id {get; set;}
+        public string ad {get; set;}
+        public string soyAd { get; set; }
+
+}
+}
